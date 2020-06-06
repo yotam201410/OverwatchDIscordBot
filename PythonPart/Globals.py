3af8decc739983e9ed01a4f31af0d5b8f8936113
yotam <yotam201410@gmail.com>
2020-06-06 16:33:19 +0300

    k

--- PythonPart/Globals.py
+++ PythonPart/Globals.py
@@ -3,4 +3,4 @@ import json
 
 class Globals:
     def __init__(self):
-        self.jsonfile = json.load(open("../Repositories/users.json", 'r', encoding="utf-8"))
+        self.jsonfile = json.load(open("../java/Repositories/users.json", 'r', encoding="utf-8"))
