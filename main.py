2b6aeeed56002a67adfd6a5d371ef8a63711cfd8
yotam <yotam201410@gmail.com>
2020-06-18 13:59:21 +0300

    making a buck up and import of the drive db

--- main.py
+++ main.py
@@ -1,12 +1,77 @@
 import os
-
+import sqlite3
 import discord
 from discord.ext import commands
-
 import sql_table_building
 from Globals import Globals
+from dateutil.parser import parse
+import datetime
 
 times = 0
+
+
+def is_date(string, fuzzy=False):
+    """
+    Return whether the string can be interpreted as a date.
+
+    :param string: str, string to check for date
+    :param fuzzy: bool, ignore unknown tokens in string if True
+    """
+    try:
+        parse(string, fuzzy=fuzzy)
+        return True
+    except ValueError:
+        return False
+
+
+def removeNone(value: tuple):
+    ret = []
+    for i in value:
+        if i == "None":
+            ret.append(None)
+        else:
+            if i.isnumeric():
+                ret.append(int(i))
+            elif is_date(i):
+                ret.append(datetime.datetime.strptime(i, "%Y-%m-%d %H:%M:%S.%f"))
+            else:
+                ret.append(i)
+
+    return tuple(ret)
+
+
+def update_data():
+    c = Globals.conn.cursor()
+    d = Globals.sheets.worksheet("server_preference").get_all_values()
+    for i in range(1, len(d)):
+        c.execute(
+            """insert into server_preference(guild_id,prefix,report_mod_channel_id,mods_role_id,helpers_role_id,join_to_create_a_room_category_id,join_to_create_a_room_channel_id,member_count_category_id,tempmute_role_id,audit_log_channel_id,commands_log_channel_id,pug_match_user_limit)
+            values (?,?,?,?,?,?,?,?,?,?,?,?)""", removeNone(d[i]))
+    d = Globals.sheets.worksheet("voice_user_data").get_all_values()
+    for i in range(1, len(d)):
+        c.execute("""insert into voice_user_data(voice_owner_id,voice_name,voice_limit)
+        values (?,?,?)""", removeNone(removeNone(d[i])))
+    d = Globals.sheets.worksheet("voice_data").get_all_values()
+    for i in range(1, len(d)):
+        c.execute("""insert into voice_data(voice_owner_id,voice_channel_id,guild_id)
+        values (?,?,?)""", removeNone(d[i]))
+    d = Globals.sheets.worksheet("member_count").get_all_values()
+    for i in range(1, len(d)):
+        c.execute("""insert into member_count(guild_id,member_count_channel_id)
+        values (?,?)""", removeNone(d[i]))
+    d = Globals.sheets.worksheet("offences").get_all_values()
+    for i in range(1, len(d)):
+        c.execute("""insert into offences(member_id,member_name,guild_id,kind,start_date,end_date,reason,treated,moderator_id)
+        values (?,?,?,?,?,?,?,?,?)""", removeNone(d[i]))
+    d = Globals.sheets.worksheet("pug_limit_5").get_all_values()
+    for i in range(1, len(d)):
+        c.execute("""insert into pug_limit_5(match_id,guild_id,red_team_player_1,red_team_player_2,red_team_player_3,red_team_player_4,red_team_player_5,blue_team_player_1,blue_team_player_2,blue_team_player_3,blue_team_player_4,blue_team_player_5,result)
+        values (?,?,?,?,?,?,?,?,?,?,?,?,?)""", removeNone(d[i]))
+    d = Globals.sheets.worksheet("pug_limit_6").get_all_values()
+    for i in range(1, len(d)):
+        c.execute("""insert into pug_limit_6(match_id,guild_id,red_team_player_1,red_team_player_2,red_team_player_3,red_team_player_4,red_team_player_5,,red_team_player_6,blue_team_player_1,blue_team_player_2,blue_team_player_3,blue_team_player_4,blue_team_player_5,blue_team_player_6,result)
+        values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)""", removeNone(d[i]))
+    Globals.conn.commit()
 
 
 def get_prefix(client, message):
@@ -63,28 +128,24 @@ async def on_command_error(ctx, error):
 async def on_ready():
     print("bot is ready v 1.0")
     c = Globals.conn.cursor()
-    try:
-        for guild in client.guilds:
-            c.execute("""INSERT INTO server_preference(guild_id,prefix)
-    SELECT :guild_id, :prefix
-    WHERE NOT EXISTS (SELECT 1 FROM server_preference WHERE guild_id = :guild_id)""",
-                      {"guild_id": guild.id, "prefix": "!"})
-    except:
-        Globals.conn.commit()
-        sql_table_building.idk()
-        c = Globals.conn.cursor()
-        for guild in client.guilds:
-            c.execute("""INSERT INTO server_preference(guild_id,prefix)
-        SELECT :guild_id, :prefix
-        WHERE NOT EXISTS (SELECT 1 FROM server_preference WHERE guild_id = :guild_id)""",
-                      {"guild_id": guild.id, "prefix": "!"})
-        Globals.conn.commit()
     global times
     if times == 0:
         for filename in os.listdir("cogs"):
             if filename.endswith(".py") and filename != "__init__.py":
                 client.load_extension(f"cogs.{filename[:-3]}")
+        try:
+            update_data()
+        except sqlite3.OperationalError:
+            sql_table_building.idk()
+            update_data()
         times += 1
+
+    for guild in client.guilds:
+        c.execute("""INSERT INTO server_preference(guild_id,prefix)
+SELECT :guild_id, :prefix
+WHERE NOT EXISTS (SELECT 1 FROM server_preference WHERE guild_id = :guild_id)""",
+                  {"guild_id": guild.id, "prefix": "!"})
+    Globals.conn.commit()
 
 
 client.run(os.environ["discord_token"])
