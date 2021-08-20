9cee11a8facb0c29b374a465371a62acdda7dad4
yotam201410 <56742172+yotam201410@users.noreply.github.com>
2021-08-20 20:12:40 +0300

    logs change more fast

--- cogs/Logs.py
+++ cogs/Logs.py
@@ -64,7 +64,7 @@ class Logs(commands.Cog):
 
     @commands.Cog.listener()
     async def on_guild_channel_delete(self, channel: discord.abc.GuildChannel):
-        if self.client.get_channel(get_audit_log_channel_id(channel.guild.id)) is not None:
+        if channel.guild.get_channel(get_audit_log_channel_id(channel.guild.id)) is not None:
             if channel.category is None:
                 date = datetime.datetime.utcnow()
                 embed = discord.Embed(title=f"a category has been deleted", timestamp=date)
@@ -77,11 +77,11 @@ class Logs(commands.Cog):
                 embed.set_author(name=self.client.user, icon_url=self.client.user.avatar_url)
                 embed.add_field(name="channel", value=str(channel), inline=False)
                 embed.add_field(name="date", value=f"{date}", inline=False)
-            await self.client.get_channel(get_audit_log_channel_id(channel.guild.id)).send(embed=embed)
+            await channel.guild.get_channel(get_audit_log_channel_id(channel.guild.id)).send(embed=embed)
 
     @commands.Cog.listener()
     async def on_guild_channel_create(self, channel: discord.abc.GuildChannel):
-        if self.client.get_channel(get_audit_log_channel_id(channel.guild.id)) is not None:
+        if channel.guild.get_channel(get_audit_log_channel_id(channel.guild.id)) is not None:
             if channel.category is None:
                 date = datetime.datetime.utcnow()
                 embed = discord.Embed(title=f"a category has been created", timestamp=date)
@@ -94,11 +94,11 @@ class Logs(commands.Cog):
                 embed.set_author(name=self.client.user, icon_url=self.client.user.avatar_url)
                 embed.add_field(name="channel", value=channel.mention, inline=False)
                 embed.add_field(name="date", value=f"{date}", inline=False)
-            await self.client.get_channel(get_audit_log_channel_id(channel.guild.id)).send(embed=embed)
+            await channel.guild.get_channel(get_audit_log_channel_id(channel.guild.id)).send(embed=embed)
 
     @commands.Cog.listener()
     async def on_guild_channel_update(self, before: discord.abc.GuildChannel, after: discord.abc.GuildChannel):
-        if self.client.get_channel(get_audit_log_channel_id(after.guild.id)) is not None:
+        if (before and before.guild.get_channel(get_audit_log_channel_id(before.guild.id)) is not None) or (after and after.guild.get_channel(get_audit_log_channel_id(after.guild.id)) is not None):
             audit_log_channel = self.client.get_channel(get_audit_log_channel_id(after.guild.id))
             if after.category is None:
                 date = datetime.datetime.utcnow()
@@ -133,41 +133,30 @@ class Logs(commands.Cog):
                     embed.add_field(name="date", value=f"{date}", inline=False)
                     await audit_log_channel.send(embed=embed)
 
-    v = {'type': 0, 'tts': False, 'timestamp': '2020-05-11T11:01:09.665000+00:00', 'pinned': False, 'mentions': [],
-         'mention_roles': [], 'mention_everyone': False, 'member':
-             {'roles': ['631032288423051297', '635912914619990027'],
-              'premium_since': None, 'nick': 'yotam201410', 'mute': False,
-              'joined_at': '2019-10-06T18:57:56.160000+00:00',
-              'hoisted_role': '631032288423051297', 'deaf': False},
-         'id': '709359439614050354', 'flags': 0, 'embeds': [], 'edited_timestamp': '2020-05-13T15:55:02.264326+00:00',
-         'content': 'https://www.youtube.com/watch?v=SOUNLig8-', 'channel_id': '650801701015781398',
-         'author': {'username': 'Yotam201410', 'public_flags': 128, 'id': '342725139626065920', 'discriminator': '6171',
-                    'avatar': '47a7a7c5898851b3b8efad9b877cc50f'},
-         'attachments': [], 'guild_id': '630411122134089747'}
 
     @commands.Cog.listener()
     async def on_member_join(self, member: discord.Member):
-        if self.client.get_channel(get_audit_log_channel_id(member.guild.id)) is not None:
+        if member.guild.get_channel(get_audit_log_channel_id(member.guild.id)) is not None:
             embed = discord.Embed(name=f"{member} has just joined the server", timestamp=datetime.datetime.utcnow())
             embed.set_author(name=f"{member}", icon_url=member.avatar_url)
             embed.add_field(name="mention", value=f"{member.mention}")
             embed.add_field(name="ID", value=f" ```python \n Member = {member.id} \n```")
-            await self.client.get_channel(get_audit_log_channel_id(member.guild.id)).send(embed=embed)
+            await member.guild.get_channel(get_audit_log_channel_id(member.guild.id)).send(embed=embed)
 
     @commands.Cog.listener()
     async def on_member_remove(self, member: discord.Member):
-        if self.client.get_channel(get_audit_log_channel_id(member.guild.id)) is not None:
+        if member.guild.get_channel(get_audit_log_channel_id(member.guild.id)) is not None:
             embed = discord.Embed(name=f"{member} has just left the server", timestamp=datetime.datetime.utcnow())
             embed.set_author(name=f"{member}", icon_url=member.avatar_url)
             embed.add_field(name="mention", value=f"{member.mention}")
             embed.add_field(name="ID", value=f"```python \n Member = {member.id} \n```")
-            await self.client.get_channel(get_audit_log_channel_id(member.guild.id)).send(embed=embed)
+            await member.guild.get_channel(get_audit_log_channel_id(member.guild.id)).send(embed=embed)
 
     @commands.Cog.listener()
     async def on_member_update(self, before: discord.Member, after: discord.Member):
-        if self.client.get_channel(get_audit_log_channel_id(after.guild.id)) is not None:
+        if before.guild.get_channel(get_audit_log_channel_id(before.guild.id)) is not None:
 
-            audit_log_channel = self.client.get_channel(get_audit_log_channel_id(after.guild.id))
+            audit_log_channel = before.guild.get_channel(get_audit_log_channel_id(before.guild.id))
             if before.nick != after.nick:
                 embed = discord.Embed(title=f"{after} had his nickname changed", timestamp=datetime.datetime.utcnow())
                 embed.set_author(name=f"{after}", icon_url=after.avatar_url)
@@ -197,21 +186,21 @@ class Logs(commands.Cog):
 
     @commands.Cog.listener()
     async def on_guild_role_create(self, role: discord.Role):
-        if self.client.get_channel(get_audit_log_channel_id(role.guild.id)) is not None:
+        if role.guild.get_channel(get_audit_log_channel_id(role.guild.id)) is not None:
             embed = discord.Embed(title=f"{role.name} has been created", timestamp=datetime.datetime.utcnow())
             embed.set_author(name=f"{self.client.user}", icon_url=self.client.user.avatar_url)
             embed.add_field(name=f"role", value=role.name, inline=False)
             embed.add_field(name="ID", value=f"````py\n Role = {role.id}")
-            await self.client.get_channel(get_audit_log_channel_id(role.guild.id)).send(embed=embed)
+            await role.guild.get_channel(get_audit_log_channel_id(role.guild.id)).send(embed=embed)
 
     @commands.Cog.listener()
     async def on_guild_role_delete(self, role: discord.Role):
-        if self.client.get_channel(get_audit_log_channel_id(role.guild.id)) is not None:
+        if role.guild.get_channel(get_audit_log_channel_id(role.guild.id)) is not None:
             embed = discord.Embed(title=f"{role.name} has been deleted", timestamp=datetime.datetime.utcnow())
             embed.set_author(name=f"{self.client.user}", icon_url=self.client.user.avatar_url)
             embed.add_field(name=f"role", value=role.name, inline=False)
             embed.add_field(name="ID", value=f"````py\n Role = {role.id}")
-            await self.client.get_channel(get_audit_log_channel_id(role.guild.id)).send(embed=embed)
+            await role.guild.get_channel(get_audit_log_channel_id(role.guild.id)).send(embed=embed)
 
     @commands.Cog.listener()
     async def on_guild_role_update(self, before: discord.Role, after: discord.Role):
@@ -221,19 +210,19 @@ class Logs(commands.Cog):
         :param before: represent the before role
         :type before: discord.Role
         """
-        if self.client.get_channel(get_audit_log_channel_id(after.guild.id)) is not None:
+        if after.guild.get_channel(get_audit_log_channel_id(after.guild.id)) is not None:
             embed = discord.Embed(title=f"{after.name} has been updated", timestamp=datetime.datetime.utcnow())
             embed.set_author(name=f"{self.client.user}", icon_url=self.client.user.avatar_url)
             embed.add_field(name=f"role", value=after.name, inline=False)
             embed.add_field(name="ID", value=f"````py\n Role = {after.id}")
-            await self.client.get_channel(get_audit_log_channel_id(after.guild.id)).send(embed=embed)
+            await after.guild.get_channel(get_audit_log_channel_id(after.guild.id)).send(embed=embed)
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member: discord.Member, before: discord.VoiceState,
                                     after: discord.VoiceState):
-        if self.client.get_channel(get_audit_log_channel_id(member.guild.id)) is not None:
+        if member.guild.get_channel(get_audit_log_channel_id(member.guild.id)) is not None:
             date = datetime.datetime.utcnow()
-            audit_log_channel = self.client.get_channel(get_audit_log_channel_id(member.guild.id))
+            audit_log_channel = member.guild.get_channel(get_audit_log_channel_id(member.guild.id))
             if after.channel != before.channel:
                 if before.channel is None:  # joined voice
                     embed = discord.Embed(timestamp=date, title=f"{member} has joined {after.channel} ")
@@ -302,9 +291,9 @@ class Logs(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_ban(self, guild: discord.Guild, user: discord.User):
-        if self.client.get_channel(get_audit_log_channel_id(guild.id)) is not None:
+        if guild.get_channel(get_audit_log_channel_id(guild.id)) is not None:
             date = datetime.datetime.utcnow()
-            audit_log_channel = self.client.get_channel(get_audit_log_channel_id(guild.id))
+            audit_log_channel = guild.get_channel(get_audit_log_channel_id(guild.id))
             embed = discord.Embed(title=f"{user} has been banned", timestamp=date)
             embed.set_author(name=f"{user}", icon_url=user.avatar_url)
             embed.add_field(name="ID", value=f"```py\n User = {user.id}\n```")
@@ -312,9 +301,9 @@ class Logs(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_unban(self, guild: discord.Guild, user: discord.User):
-        if self.client.get_channel(get_audit_log_channel_id(guild.id)) is not None:
+        if guild.get_channel(get_audit_log_channel_id(guild.id)) is not None:
             date = datetime.datetime.utcnow()
-            audit_log_channel = self.client.get_channel(get_audit_log_channel_id(guild.id))
+            audit_log_channel = guild.get_channel(get_audit_log_channel_id(guild.id))
             embed = discord.Embed(title=f"{user} has been unbanned", timestamp=date)
             embed.set_author(name=f"{user}", icon_url=user.avatar_url)
             embed.add_field(name="ID", value=f"```py\n User = {user.id}\n```")
